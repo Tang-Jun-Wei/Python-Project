b229689562dc9bbe5bb11ef658822fcd7258cdab
yy176 <yy17608471236@163.com>
2025-07-23 21:34:14 +0800

    你是个好人！

--- hello.py
+++ hello.py
@@ -1,11 +1,13 @@
-# This program says hello and asks for my name.
-
-print('Hello world!')
-print('What is your name?') # ask for their name
-myName = input()
-print('It is good to meet you, ' + myName)
-print('The length of your name is:')
-print(len(myName))
-print('What is your age?') # ask for their age
-myAge = input()
-print('You will be ' + str(int(myAge) + 1) + ' in a year.')
+# # This program says hello and asks for my name.
+#
+# print('Hello world!')
+# print('What is your name?') # ask for their name
+# myName = input()
+# print('It is good to meet you, ' + myName)
+# print('The length of your name is:')
+# print(len(myName))
+# print('What is your age?') # ask for their age
+# myAge = input()
+# print('You will be ' + str(int(myAge) + 1) + ' in a year.')
+sb=str(input('唐俊伟是什么：'))
+print( f'唐俊伟是{sb}!' if sb  == 'sb' else '唐俊伟是sb!' , sep="\n")
