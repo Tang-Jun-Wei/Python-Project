3dc0a7f841ad6a14d215391a862b51442557c92c
yy176 <yy17608471236@163.com>
2025-07-23 22:59:16 +0800

    余益是我sun

--- hello.py
+++ hello.py
@@ -9,5 +9,5 @@
 # print('What is your age?') # ask for their age
 # myAge = input()
 # print('You will be ' + str(int(myAge) + 1) + ' in a year.')
-sb=str(input('唐俊伟是什么：'))
-print( f'唐俊伟是{sb}!' if sb  == 'sb' else '唐俊伟是sb!' , sep="\n")
+sb=str(input('余益是什么：'))
+print( f'余益是{sb}!' if sb  == 'sb' else '唐俊伟是余益爸爸!' , sep="\n")
